f1f74b45cb8c3731ce09f4fa2f0c795a5961bfef
Tilak747 <c0868747@mylambton.ca>
2023-02-13 18:06:09 -0500

    Initial commit

---
+++ app/src/main/java/c0868747/tilak/finaltestandroid/MyApp.java
@@ -0,0 +1,14 @@
+package c0868747.tilak.finaltestandroid;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
